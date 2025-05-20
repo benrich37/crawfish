b380b0c42d75c500a287d8905728cba90dce4fc2
benrich37 <benjamin.rich@colorado.edu>
2025-05-19 19:09:09 -0600

    Uses a super generalized dictionary comparison dispatcher to check matching metadata

--- src/crawfish/funcs/general.py
+++ src/crawfish/funcs/general.py
@@ -7,10 +7,15 @@ from crawfish.core.elecdata import ElecData
 from crawfish.core.operations.vector import get_gauss_smear_spectrum, get_lti_spectrum, get_uneven_integrated_array
 from crawfish.utils.arg_correction import get_erange
 from crawfish.utils.typing import REAL_DTYPE, cs_formatter, COMPLEX_DTYPE
+from crawfish.utils.caching import get_spectrum_file_path
 from copy import deepcopy
 import numpy as np
 from scipy.integrate import trapezoid
 from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from typing import Any
 
 SIGMA_DEFAULT = REAL_DTYPE(0.00001)
 RES_DEFAULT = REAL_DTYPE(0.01)
@@ -54,261 +59,6 @@ def get_generic_integrate(
     return es, cs
 
 
-def parse_generic_metadata(cache_dir: Path) -> dict:
-    """Parse the metadata from the cache directory.
-
-    Parse the metadata from the cache directory.
-    """
-    metadata_file = cache_dir / "metadata.txt"
-    vals = {}
-    with open(metadata_file, "r") as f:
-        for line in f:
-            if ":" in line:
-                try:
-                    key, val = line.split(":")
-                    vals[key.strip()] = val.split("#")[0]
-                except TypeError:
-                    pass
-    return vals
-
-def write_generic_metadata(cache_dir: Path, metadata_dict: dict):
-    """Write the metadata to the cache directory.
-
-    Write the metadata to the cache directory.
-    """
-    metadata_file = cache_dir / "metadata.txt"
-    with open(metadata_file, "w") as f:
-        for key, val in metadata_dict.items():
-            f.write(f"{key}: {val}\n")
-
-
-def parse_erange_from_erange_cache_dir(erange_cache_dir: Path) -> tuple[float, float, float]:
-    """Parse the energy range from the energy range cache directory.
-
-    Parse the energy range from the energy range cache directory.
-    """
-    metadata_dict = parse_generic_metadata(erange_cache_dir)
-    if all([key in metadata_dict for key in ["emin", "emax", "estep"]]):
-        emin = float(metadata_dict["emin"])
-        emax = float(metadata_dict["emax"])
-        estep = float(metadata_dict["estep"])
-        return emin, emax, estep
-    else:
-        return None, None, None
-    
-
-def is_generic_cache_dir(test_cache_dir: Path, metadata_dict: dict) -> bool:
-    test_metadata_dict = parse_generic_metadata(test_cache_dir)
-    for key, val in metadata_dict.items():
-        if key not in test_metadata_dict:
-            return False
-        if not test_metadata_dict[key].strip() == str(val).strip():
-            return False
-    return True
-
-
-def is_erange_cache_dir(erange_cache_dir: Path, emin: float, emax: float, estep: float) -> bool:
-    emin, emax, estep = parse_erange_from_erange_cache_dir(erange_cache_dir)
-    if None in [emin, emax, estep]:
-        return False
-    if all([np.isclose(emin, emin), np.isclose(emax, emax), np.isclose(estep, estep)]):
-        return True
-    return
-
-def get_preexisting_generic_cache_dirs(parent_dir: Path, prefix: str):
-    cache_dirs = list(parent_dir.glob(f"{prefix}_*"))
-    cache_dirs = [d for d in cache_dirs if d.is_dir()]
-    return cache_dirs
-
-def get_preexisting_erange_cache_dirs(edata: ElecData, erange: np.ndarray):
-    return get_preexisting_generic_cache_dirs(edata.cache_sub_dir, "erange")
-
-def find_generic_cache_dir(parent_dir: Path, prefix: str, metadata_dict: dict):
-    """Find the generic cache directory.
-    Find the generic cache directory.
-    """
-    cache_dirs = get_preexisting_generic_cache_dirs(parent_dir, prefix)
-    for cache_dir in cache_dirs:
-        if is_generic_cache_dir(cache_dir, metadata_dict):
-            return cache_dir
-    return None
-
-def find_erange_cache_dir(edata: ElecData, erange: np.ndarray):
-    """Find the energy range cache directory.
-
-    Find the energy range cache directory.
-    """
-    erange_cache_dirs = get_preexisting_erange_cache_dirs(edata, erange)
-    emin = erange[0]
-    emax = erange[-1]
-    estep = erange[1] - erange[0]
-    for erange_cache_dir in erange_cache_dirs:
-        if is_erange_cache_dir(erange_cache_dir, emin, emax, estep):
-            return erange_cache_dir
-    return None
-
-def write_new_erange_cache_dir(edata: ElecData, erange: np.ndarray):
-    """Write a new energy range cache directory.
-
-    Write a new energy range cache directory.
-    """
-    parent_cache_dir = edata.cache_sub_dir
-    metadata_dict = {
-        "emin": erange[0],
-        "emax": erange[-1],
-        "estep": erange[1] - erange[0],
-    }
-    return write_new_generic_cache_dir(parent_cache_dir, "erange", metadata_dict)
-
-
-def write_new_generic_cache_dir(parent_dir: Path, prefix: str, metadata_dict: dict):
-    """Write a new energy range cache directory.
-
-    Write a new energy range cache directory.
-    """
-    cache_dirs = get_preexisting_generic_cache_dirs(parent_dir, prefix)
-    cache_dir = None
-    for i in range(len(cache_dirs)+1):
-        cache_dir = parent_dir / f"{prefix}_{i}"
-        if not cache_dir.is_dir():
-            break
-    if cache_dir is None:
-        raise ValueError(f"Issue with get_preexisting_generic_cache_dirs not returning all valid {prefix} cache dirs in {parent_dir}.")
-    cache_dir.mkdir(parents=True, exist_ok=True)
-    write_generic_metadata(cache_dir, metadata_dict)
-    return cache_dir
-
-
-def get_erange_cache_dir(edata: ElecData, erange: np.ndarray):
-    """Return the energy range cache directory.
-
-    Return the energy range cache directory.
-    """
-    erange_cache_dir = find_erange_cache_dir(edata, erange)
-    if erange_cache_dir is not None:
-        return erange_cache_dir
-    else:
-        erange_cache_dir = write_new_erange_cache_dir(edata, erange)
-        return erange_cache_dir
-    
-def get_generic_cache_dir(parent_dir: Path, prefix: str, metadata_dict: dict):
-    """Return the generic cache directory.
-
-    Return the generic cache directory.
-    """
-    cache_dir = find_generic_cache_dir(parent_dir, prefix, metadata_dict)
-    if cache_dir is not None:
-        return cache_dir
-    else:
-        cache_dir = write_new_generic_cache_dir(parent_dir, prefix, metadata_dict)
-        return cache_dir
-
-
-def get_spectrum_file_name_func_args_prefix(func_args_dict: dict) -> str:
-    """Return the spectrum file name function arguments prefix.
-
-    Return the spectrum file name function arguments prefix. Gives portion
-    of file name that encodes the function arguments.
-    """
-    # Sort the keys by alphabetical order
-    sorted_keys = sorted(func_args_dict.keys())
-    sorted_vals = [str(func_args_dict[key]) for key in sorted_keys]
-    # Join the sorted keys and values into a string
-    func_args_str = "_".join([f"{key}_{val}" for key, val in zip(sorted_keys, sorted_vals)])
-    # Remove any whitespace
-    func_args_str = func_args_str.replace(" ", "")
-    return func_args_str
-
-def get_spectrum_file_name_spectrum_args_suffix(
-    spin_pol: bool = False,
-    sig: REAL_DTYPE = SIGMA_DEFAULT,
-    lti: bool = False,
-    rattle_eigenvals: bool = False,
-    norm_max: bool = False,
-    norm_intg: bool = False,
-    sep_channels: bool = False,
-):
-    """Return the spectrum file name spectrum arguments suffix.
-
-    Return the spectrum file name spectrum arguments suffix. Gives portion
-    of file name that encodes the spectrum evaluation arguments.
-    """
-    p1 = f"lti_{lti}"
-    if lti:
-        p1 += f"_rattle_{rattle_eigenvals}"
-    else:
-        p1 += f"_sig_{sig}"
-    p2s = [
-        f"norm_max_{norm_max}",
-        f"norm_intg_{norm_intg}",
-        f"sep_channels_{sep_channels}",
-        f"spin_pol_{spin_pol}",
-    ]
-    p2 = "_".join([p for p in p2s])
-    return f"{p1}_{p2}"
-
-        
-    
-
-def get_spectrum_file_name(
-    func_args_dict: dict,
-    spin_pol: bool = False,
-    sig: REAL_DTYPE = SIGMA_DEFAULT,
-    lti: bool = False,
-    rattle_eigenvals: bool = False,
-    norm_max: bool = False,
-    norm_intg: bool = False,
-    sep_channels: bool = False,
-    ) -> str:
-    """Return the spectrum file name.
-
-    Return the spectrum file name.
-    """
-    prefix = get_spectrum_file_name_func_args_prefix(func_args_dict)
-    suffix = get_spectrum_file_name_spectrum_args_suffix(
-        spin_pol=spin_pol,
-        sig=sig,
-        lti=lti,
-        rattle_eigenvals=rattle_eigenvals,
-        norm_max=norm_max,
-        norm_intg=norm_intg,
-        sep_channels=sep_channels,
-    )
-    return f"{prefix}_{suffix}.npy"
-
-
-def get_spectrum_file_path(
-    edata: ElecData,
-    func_name: str,
-    func_args_dict: dict,
-    erange: np.ndarray[REAL_DTYPE] | None = None,
-    spin_pol: bool = False,
-    sig: REAL_DTYPE = SIGMA_DEFAULT,
-    res: REAL_DTYPE = RES_DEFAULT,
-    lti: bool = False,
-    rattle_eigenvals: bool = False,
-    norm_max: bool = False,
-    norm_intg: bool = False,
-    sep_channels: bool = False,
-):
-    if erange is None:
-        erange = get_erange(edata, erange, res=res)
-    erange_cache_dir = get_erange_cache_dir(edata, erange)
-    metadata_dict = func_args_dict.copy()
-    metadata_dict["spin_pol"] = spin_pol
-    metadata_dict["sig"] = sig
-    metadata_dict["lti"] = lti
-    metadata_dict["rattle_eigenvals"] = rattle_eigenvals
-    metadata_dict["norm_max"] = norm_max
-    metadata_dict["norm_intg"] = norm_intg
-    metadata_dict["sep_channels"] = sep_channels
-    spec_cache_dir = get_generic_cache_dir(
-        erange_cache_dir,
-        func_name,
-        metadata_dict,
-    )
-    file_path = spec_cache_dir / "spec.npy"
-    return file_path
 
 def evaluate_or_retrieve_generic_spectrum(
     edata: ElecData,
@@ -362,8 +112,6 @@ def evaluate_or_retrieve_generic_spectrum(
     if erange is None:
         erange = get_erange(edata, erange, res=res)
     return erange, spectrum
-
-
 
 
 def get_generic_spectrum(
--- src/crawfish/utils/caching.py
+++ src/crawfish/utils/caching.py
@@ -4,6 +4,11 @@ from functools import lru_cache
 from pathlib import Path
 from crawfish.utils.typing import REAL_DTYPE
 from crawfish.io.general import safe_load
+from typing import Any
+from crawfish.core.elecdata import ElecData
+from crawfish.utils.arg_correction import get_erange
+from crawfish.utils.typing import REAL_DTYPE, COMPLEX_DTYPE
+from crawfish.utils.typing import cs_formatter
 
 
 
@@ -77,11 +82,230 @@ class CachedFunction:
             self.cache = dict(zip(keys, values))
             for key in keys:
                 self.cache[key] = np.array(self.cache[key], dtype=self.arr_dtype)
+
+
+def parse_generic_metadata(cache_dir: Path) -> dict:
+    """Parse the metadata from the cache directory.
+
+    Parse the metadata from the cache directory.
+    """
+    metadata_file = cache_dir / "metadata.txt"
+    vals = {}
+    with open(metadata_file, "r") as f:
+        for line in f:
+            vals = eval(line.strip())
+            break
+    return vals
+
+def write_generic_metadata(cache_dir: Path, metadata_dict: dict):
+    """Write the metadata to the cache directory.
+
+    Write the metadata to the cache directory.
+    """
+    metadata_file = cache_dir / "metadata.txt"
+    with open(metadata_file, "w") as f:
+        f.write(str(metadata_dict))
     
-    # def load_cache(self, filename: str):
-    #     """Load cache from a file"""
-    #     if Path(filename).exists():
-    #         loaded = np.load(filename, allow_pickle=True)
-    #         keys = loaded['keys']
-    #         values = loaded['values']
-    #         self.cache = dict(zip(keys, values))
+
+def is_matching_str(v_from_file: str, v_from_args: str) -> bool:
+    """Check if the string values match.
+
+    Check if the string values match.
+    """
+    return v_from_file.strip() == v_from_args.strip()
+
+
+def is_matching_float(v_from_file: str, v_from_args: float) -> bool:
+    """Check if the float values match.
+
+    Check if the float values match.
+    """
+    try:
+        v_from_file = float(v_from_file)
+    except (ValueError, TypeError):
+        return False
+    return np.isclose(v_from_file, v_from_args)
+
+def is_matching_int(v_from_file: str, v_from_args: int) -> bool:
+    """Check if the int values match.
+
+    Check if the int values match.
+    """
+    try:
+        v_from_file = int(v_from_file)
+    except (ValueError, TypeError):
+        return False
+    return v_from_file == v_from_args
+
+def is_matching_bool(v_from_file: str, v_from_args: bool) -> bool:
+    """Check if the bool values match.
+
+    Check if the bool values match.
+    """
+    try:
+        v_from_file = bool(v_from_file)
+    except (ValueError, TypeError):
+        return False
+    return v_from_file == v_from_args
+
+
+def list_contains_val(test_list: list[Any], val: Any) -> bool:
+    """Check if the list contains the value.
+
+    Check if the list contains the value.
+    """
+    return any([is_matching_value(v, val) for v in test_list])
+
+def is_matching_list(v_from_file: Any, v_from_args: list[Any]) -> bool:
+    """Check if the list values match.
+
+    Check if the list values match.
+    """
+    try:
+        v_from_file = eval(str(v_from_file))
+    except (ValueError, TypeError):
+        return False
+    if len(v_from_file) != len(v_from_args):
+        return False
+    for subv in v_from_args:
+        if not list_contains_val(v_from_file, subv):
+            return False
+    return True
+
+def is_matching_value(
+    v_from_file: Any,
+    v_from_args: Any,
+) -> bool:
+    """Check if the values match.
+
+    Check if the values match.
+    """
+    if isinstance(v_from_args, str):
+        return is_matching_str(v_from_file, v_from_args)
+    elif isinstance(v_from_args, float):
+        return is_matching_float(v_from_file, v_from_args)
+    elif isinstance(v_from_args, int):
+        return is_matching_int(v_from_file, v_from_args)
+    elif isinstance(v_from_args, bool):
+        return is_matching_bool(v_from_file, v_from_args)
+    elif isinstance(v_from_args, list):
+        return is_matching_list(v_from_file, v_from_args)
+    elif isinstance(v_from_args, dict):
+        return is_matching_metadata(v_from_file, v_from_args)
+    else:
+        raise ValueError(f"Unsupported type {type(v_from_args)} for value {v_from_args}.")
+    
+
+def is_matching_metadata(
+    metadata_from_file: dict[str, Any],
+    metadata_from_args: dict[str, Any]
+) -> bool:
+    """Check if the metadata in the cache directory matches the given metadata.
+
+    Check if the metadata in the cache directory matches the given metadata.
+    """
+    for key, val in metadata_from_args.items():
+        if key not in metadata_from_file:
+            return False
+        elif not is_matching_value(metadata_from_file[key], val):
+            return False
+    return True
+
+    
+def is_generic_cache_dir(test_cache_dir: Path, metadata_dict: dict) -> bool:
+    test_metadata_dict = parse_generic_metadata(test_cache_dir)
+    return is_matching_metadata(test_metadata_dict, metadata_dict)
+
+
+def get_preexisting_generic_cache_dirs(parent_dir: Path, prefix: str):
+    cache_dirs = list(parent_dir.glob(f"{prefix}_*"))
+    cache_dirs = [d for d in cache_dirs if d.is_dir()]
+    return cache_dirs
+
+def find_generic_cache_dir(parent_dir: Path, prefix: str, metadata_dict: dict):
+    """Find the generic cache directory.
+    Find the generic cache directory.
+    """
+    cache_dirs = get_preexisting_generic_cache_dirs(parent_dir, prefix)
+    for cache_dir in cache_dirs:
+        if is_generic_cache_dir(cache_dir, metadata_dict):
+            return cache_dir
+    return None
+
+
+def write_new_generic_cache_dir(parent_dir: Path, prefix: str, metadata_dict: dict):
+    """Write a new energy range cache directory.
+
+    Write a new energy range cache directory.
+    """
+    cache_dirs = get_preexisting_generic_cache_dirs(parent_dir, prefix)
+    cache_dir = None
+    for i in range(len(cache_dirs)+1):
+        cache_dir = parent_dir / f"{prefix}_{i}"
+        if not cache_dir.is_dir():
+            break
+    if cache_dir is None:
+        raise ValueError(f"Issue with get_preexisting_generic_cache_dirs not returning all valid {prefix} cache dirs in {parent_dir}.")
+    cache_dir.mkdir(parents=True, exist_ok=True)
+    write_generic_metadata(cache_dir, metadata_dict)
+    return cache_dir
+
+    
+def get_generic_cache_dir(parent_dir: Path, prefix: str, metadata_dict: dict):
+    """Return the generic cache directory.
+
+    Return the generic cache directory.
+    """
+    cache_dir = find_generic_cache_dir(parent_dir, prefix, metadata_dict)
+    if cache_dir is not None:
+        return cache_dir
+    else:
+        cache_dir = write_new_generic_cache_dir(parent_dir, prefix, metadata_dict)
+        return cache_dir
+
+def get_erange_cache_dir(edata: ElecData, erange: np.ndarray):
+    """Return the energy range cache directory.
+
+    Return the energy range cache directory.
+    """
+    parent_dir = edata.cache_sub_dir
+    prefix = "erange"
+    metadata_dict = {
+        "emin": erange[0],
+        "emax": erange[-1],
+        "nstep": len(erange),
+    }
+    return get_generic_cache_dir(parent_dir, prefix, metadata_dict)
+
+def get_spectrum_file_path(
+    edata: ElecData,
+    func_name: str,
+    func_args_dict: dict,
+    erange: np.ndarray[REAL_DTYPE] | None = None,
+    spin_pol: bool = False,
+    sig: REAL_DTYPE = 0.0,
+    res: REAL_DTYPE = 0.0,
+    lti: bool = False,
+    rattle_eigenvals: bool = False,
+    norm_max: bool = False,
+    norm_intg: bool = False,
+    sep_channels: bool = False,
+):
+    if erange is None:
+        erange = get_erange(edata, erange, res=res)
+    erange_cache_dir = get_erange_cache_dir(edata, erange)
+    metadata_dict = func_args_dict.copy()
+    metadata_dict["spin_pol"] = spin_pol
+    metadata_dict["sig"] = sig
+    metadata_dict["lti"] = lti
+    metadata_dict["rattle_eigenvals"] = rattle_eigenvals
+    metadata_dict["norm_max"] = norm_max
+    metadata_dict["norm_intg"] = norm_intg
+    metadata_dict["sep_channels"] = sep_channels
+    spec_cache_dir = get_generic_cache_dir(
+        erange_cache_dir,
+        func_name,
+        metadata_dict,
+    )
+    file_path = spec_cache_dir / "spec.npy"
+    return file_path
